a2bb0ac780bbc8bbb7b88af2051ec30e1496ec95
sameernshamsudheen <94037043+sameernshamsudheen@users.noreply.github.com>
2024-09-29 19:39:42 +0530

    initialcommit

---
+++ server.js
@@ -0,0 +1,9 @@
+
+const  express =require("express")
+require('dotenv').config()
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`server Started at ${process.env.PORT}`);
+});
